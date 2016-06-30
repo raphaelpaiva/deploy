eec066f9eb225e76f1b75e626c1083cbf5ad7e91
Raphael Paiva <raphaeldpaiva@gmail.com>
2016-06-30 18:02:43 -0300

    Making tests easier to run.

--- test.sh
+++ test.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+TEST_PATTERN="*_tests.py"
+JBOSSCLI_PATTERN="jbosscli*"
+
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     PATH=$PATH:/c/Python27
 fi
 
-python -m unittest `ls *_tests.py | sed 's/\.py//g'`
+rm -f .coverage
+
+for f in $TEST_PATTERN; do
+  python -m coverage run --omit=$TEST_PATTERN,$JBOSSCLI_PATTERN -a $f
+done
+
+python -m coverage html
+
+#python -m unittest `ls *_tests.py | sed 's/\.py//g'`
