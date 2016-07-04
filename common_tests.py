8e2982d7ad48da0a4a58168f7f2ea62ae3560ed8
Raphael Paiva <raphaeldpaiva@gmail.com>
2016-07-04 11:02:12 -0300

    Adding tests to common module.

---
+++ common_tests.py
@@ -0,0 +1,23 @@
+import unittest
+from mock import MagicMock
+from mock import patch
+from StringIO import StringIO
+
+import common
+
+@patch("sys.stdout", new_callable=StringIO)
+class CommonTests(unittest.TestCase):
+    def test_initialize_controller(self, mock_stdout):
+        args = MagicMock()
+        args.controller = "host:port"
+        args.auth = "user:passw"
+
+        value = common.initialize_controller(args)
+
+        self.assertIsNone(value)
+        self.assertEquals(mock_stdout.getvalue(), "Failed to parse: host:port\n")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
