920a240b867abb339575066bf995550c8c947b59
Raphael Paiva <raphaeldpaiva@gmail.com>
2016-07-25 15:53:32 -0300

    Adding tests to verify_deployments.

--- verify_deployments.py
+++ verify_deployments.py
@@ -3,8 +3,9 @@ import os
 import common
 import deploy
 
-INITIALIZE_ERROR_RET_CODE = 2
-OK_RETURN_CODE            = 0
+OK_RET_CODE         = 0
+ERROR_RET_CODE      = 1
+INIT_ERROR_RET_CODE = 2
 
 def verify(args):
     path = os.path.abspath(args.path) + os.sep
@@ -12,7 +13,7 @@ def verify(args):
 
     controller = common.initialize_controller(args)
     if controller is None:
-        return ("# Cannot reach controller {0}.".format(args.controller), INITIALIZE_ERROR_RET_CODE)
+        return ("# Cannot reach controller {0}.".format(args.controller), INIT_ERROR_RET_CODE)
 
     output = ""
 
@@ -20,19 +21,26 @@ def verify(args):
 
     to_be_deployed = deploy.read_archive_files(path, tag)
 
+    if not to_be_deployed:
+        return ("Deployment directory is empty!", INIT_ERROR_RET_CODE)
+
     deployed_names = {x.name for x in enabled_deployments}
     to_be_deployed_names = {x.name for x in to_be_deployed}
 
     everything_deployed = verify_deployments(to_be_deployed_names, deployed_names)
 
+    return_code = None
+
     if everything_deployed:
         output += "OK!"
+        return_code = OK_RET_CODE
     else:
         not_deployed = to_be_deployed_names - deployed_names
         output += "ERROR: some modules were not deployed:\n"
         output += "\n".join(not_deployed)
+        return_code = ERROR_RET_CODE
 
-    return (output, OK_RETURN_CODE)
+    return (output, return_code)
 
 def verify_deployments(to_be, deployed):
     return to_be.issubset(deployed)
---
+++ verify_deployments_tests.py
@@ -0,0 +1,54 @@
+import unittest
+
+from mock import MagicMock
+from mock import patch
+
+from jbosscli import Deployment
+
+import verify_deployments
+
+deployment_app = Deployment("app-1.2.3", "app.war", enabled=True)
+deployment_ppa = Deployment("ppa-1.3.2", "ppa.war", enabled=True)
+
+class VerifyDeploymentsTests(unittest.TestCase):
+    @patch("verify_deployments.deploy.read_archive_files", MagicMock(return_value=[]))
+    @patch("verify_deployments.common.fetch_enabled_deployments", MagicMock(return_value=[deployment_app, deployment_ppa]))
+    @patch("verify_deployments.common.initialize_controller", MagicMock())
+    @patch("verify_deployments.os.path.abspath", MagicMock(return_value="/tmp/deployment"))
+    def test_verify_emptyFolder_shouldReturn2AndHaveErrorMessage(self):
+        args = MagicMock()
+        result = verify_deployments.verify(args)
+        output = result[0]
+        return_code = result[1]
+
+        self.assertEquals(return_code, 2)
+        self.assertEquals(output, "Deployment directory is empty!")
+
+    @patch("verify_deployments.deploy.read_archive_files", MagicMock(return_value=[deployment_app, deployment_ppa]))
+    @patch("verify_deployments.common.fetch_enabled_deployments", MagicMock(return_value=[deployment_app]))
+    @patch("verify_deployments.common.initialize_controller", MagicMock())
+    @patch("verify_deployments.os.path.abspath", MagicMock(return_value="/tmp/deployment"))
+    def test_verify_MissingDeploymentInServer_shouldReturn1AndHaveErrorMessage(self):
+        args = MagicMock()
+        result = verify_deployments.verify(args)
+        output = result[0]
+        return_code = result[1]
+
+        self.assertEquals(return_code, 1)
+        self.assertEquals(output, "ERROR: some modules were not deployed:\nppa-1.3.2")
+
+    @patch("verify_deployments.deploy.read_archive_files", MagicMock(return_value=[deployment_app]))
+    @patch("verify_deployments.common.fetch_enabled_deployments", MagicMock(return_value=[deployment_app, deployment_ppa]))
+    @patch("verify_deployments.common.initialize_controller", MagicMock())
+    @patch("verify_deployments.os.path.abspath", MagicMock(return_value="/tmp/deployment"))
+    def test_verify_emptyFolder_shouldReturn0AndHaveOKMessage(self):
+        args = MagicMock()
+        result = verify_deployments.verify(args)
+        output = result[0]
+        return_code = result[1]
+
+        self.assertEquals(return_code, 0)
+        self.assertEquals(output, "OK!")
+
+if __name__ == "__main__":
+    unittest.main()
