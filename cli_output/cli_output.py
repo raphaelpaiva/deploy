4be11dab75f743a55fa1b67d781709fffcf172c8
Raphael Paiva <raphaeldpaiva@gmail.com>
2016-08-31 16:55:58 -0300

    Refactor cli_output module for compliance with pep8

--- cli_output/cli_output.py
+++ cli_output/cli_output.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python
 
+
 def prepare_deploy_statement(deployment):
     deployment_statement = "deploy {0} --runtime-name={1} --name={2}{3}"
-    server_group_statement = "" if not deployment.server_group else " --server-groups={0}".format(deployment.server_group)
+    server_group_statement = "" if not deployment.server_group else \
+        " --server-groups={0}".format(deployment.server_group)
 
     deployment_path = deployment.path if deployment.path else ""
 
-    return deployment_statement.format(deployment_path, deployment.runtime_name, deployment.name, server_group_statement)
+    return deployment_statement.format(
+        deployment_path, deployment.runtime_name,
+        deployment.name, server_group_statement
+    )
+
 
 def prepare_undeploy_statement(deployment, undeploy_tag=None):
     undeploy_statement = "undeploy {0} --keep-content{1}"
-    server_group_statement = " --all-relevant-server-groups" if deployment.server_group else ""
+    server_group_statement = " --all-relevant-server-groups" \
+        if deployment.server_group else ""
 
-    name = deployment.name if not undeploy_tag else deployment.runtime_name.replace(".war", "").replace(".jar", "") + "-" + undeploy_tag
+    name = deployment.name if not undeploy_tag else \
+        deployment.runtime_name.replace(".war", "").replace(".jar", "") + \
+        "-" + undeploy_tag
 
     return undeploy_statement.format(name, server_group_statement)
+
 
 def generate_undeploy_pattern(undeploy_pattern):
     return "undeploy --name=" + undeploy_pattern + " --keep-content"
 
+
 def generate_undeploy_script(archives, undeploy_tag=None):
     script = ""
     for archive in archives:
-        script = script + "\n" + prepare_undeploy_statement(archive, undeploy_tag)
+        script = script + "\n" + \
+            prepare_undeploy_statement(archive, undeploy_tag)
 
     return script
+
 
 def generate_deploy_script(archives):
     script = ""
--- cli_output/cli_output_tests.py
+++ cli_output/cli_output_tests.py
@@ -3,40 +3,55 @@ import unittest
 import cli_output
 from jbosscli import Deployment
 
+
 class CliOutputTests(unittest.TestCase):
-  def test_prepare_deploy_statement_warfile(self):
-      path = os.path.join("/tmp", "deploy") + os.path.sep + "archive.war"
-      deployment = Deployment("archive-notag", "archive.war", path=path)
 
-      actual_statement = cli_output.prepare_deploy_statement(deployment)
-      expected_statement = 'deploy ' + path + ' --runtime-name=archive.war --name=archive-notag'
+    def test_prepare_deploy_statement_warfile(self):
+        path = os.path.join("/tmp", "deploy") + os.path.sep + "archive.war"
+        deployment = Deployment("archive-notag", "archive.war", path=path)
 
-      self.assertEqual(actual_statement, expected_statement)
+        actual_statement = cli_output.prepare_deploy_statement(deployment)
+        expected_statement = 'deploy ' + path + \
+            ' --runtime-name=archive.war --name=archive-notag'
 
-  def test_prepare_deploy_statement_jarfile(self):
-      path = os.path.join("/tmp", "deploy") + os.path.sep + "archive.jar"
-      deployment = Deployment("archive-notag", "archive.jar", path=path)
+        self.assertEqual(actual_statement, expected_statement)
 
-      actual_statement = cli_output.prepare_deploy_statement(deployment)
-      expected_statement = 'deploy ' + path + ' --runtime-name=archive.jar --name=archive-notag'
+    def test_prepare_deploy_statement_jarfile(self):
+        path = os.path.join("/tmp", "deploy") + os.path.sep + "archive.jar"
+        deployment = Deployment("archive-notag", "archive.jar", path=path)
 
-      self.assertEqual(actual_statement, expected_statement)
+        actual_statement = cli_output.prepare_deploy_statement(deployment)
+        expected_statement = 'deploy ' + path + \
+            ' --runtime-name=archive.jar --name=archive-notag'
 
-  def test_prepare_undeploy_statement_warfile(self):
-      deployment = Deployment("archive-tag", "archive.war")
-      self.assertEqual(cli_output.prepare_undeploy_statement(deployment), "undeploy archive-tag --keep-content")
+        self.assertEqual(actual_statement, expected_statement)
 
-  def test_prepare_undeploy_statement_warfile_withUndeployTag(self):
-      deployment = Deployment("archive-tag", "archive.war")
-      self.assertEqual(cli_output.prepare_undeploy_statement(deployment, "undeploy_tag"), "undeploy archive-undeploy_tag --keep-content")
+    def test_prepare_undeploy_statement_warfile(self):
+        deployment = Deployment("archive-tag", "archive.war")
+        self.assertEqual(cli_output.prepare_undeploy_statement(deployment),
+                         "undeploy archive-tag --keep-content")
 
-  def test_prepare_undeploy_statement_jarfile_withUndeployTag(self):
-      deployment = Deployment("archive-tag", "archive.jar")
-      self.assertEqual(cli_output.prepare_undeploy_statement(deployment, "undeploy_tag"), "undeploy archive-undeploy_tag --keep-content")
+    def test_prepare_undeploy_statement_warfile_withUndeployTag(self):
+        deployment = Deployment("archive-tag", "archive.war")
 
-  def test_prepare_undeploy_statement_jarfile(self):
-      deployment = Deployment("archive-tag", "archive.jar")
-      self.assertEqual(cli_output.prepare_undeploy_statement(deployment), "undeploy archive-tag --keep-content")
+        self.assertEqual(
+            cli_output.prepare_undeploy_statement(deployment, "undeploy_tag"),
+            "undeploy archive-undeploy_tag --keep-content"
+        )
+
+    def test_prepare_undeploy_statement_jarfile_withUndeployTag(self):
+        deployment = Deployment("archive-tag", "archive.jar")
+        self.assertEqual(
+            cli_output.prepare_undeploy_statement(deployment, "undeploy_tag"),
+            "undeploy archive-undeploy_tag --keep-content"
+        )
+
+    def test_prepare_undeploy_statement_jarfile(self):
+        deployment = Deployment("archive-tag", "archive.jar")
+        self.assertEqual(
+            cli_output.prepare_undeploy_statement(deployment),
+            "undeploy archive-tag --keep-content"
+        )
 
 if __name__ == "__main__":
     unittest.main()
