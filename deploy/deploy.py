6066704745cce53857bcb2c2ad3dc2b10e09d963
Raphael Paiva <raphaeldpaiva@gmail.com>
2016-08-31 17:48:00 -0300

    Refactor deploy module for compliance with pep8

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -6,6 +6,7 @@ import cli_output
 import jbosscli
 import common
 
+
 def read_server_group_mapping(mapping_file):
     """Given a mapping file path, read it and return a dict with its contents.
 
@@ -28,11 +29,18 @@ def read_server_group_mapping(mapping_file):
 
     return mapping
 
+
 def map_server_groups(archives, mapping):
-    """Given the archives and a server group mapping dict, update the archive's server_group property."""
+    """Given the archives and a server group mapping dict,
+    update the archive's server_group property.
+    """
     for archive in archives:
         if archive.server_group is None and archive.runtime_name in mapping:
-            archive.server_group = jbosscli.ServerGroup(mapping[archive.runtime_name], None)
+            archive.server_group = jbosscli.ServerGroup(
+                mapping[archive.runtime_name],
+                None
+            )
+
 
 def generate_deploy_script(args, rollback_file_dir=None):
     path = os.path.abspath(args.path) + os.sep
@@ -51,16 +59,29 @@ def generate_deploy_script(args, rollback_file_dir=None):
     undeploy_script = ""
 
     if controller:
-        enabled_deployments = common.fetch_enabled_deployments(controller, archives)
+        enabled_deployments = common.fetch_enabled_deployments(
+            controller,
+            archives
+        )
 
-        rollback_filename_template = common.generate_rollback_filename_template(args.rollback_info_file_suffix)
-        rollback_info_file = persist_rollback_info(enabled_deployments, rollback_filename_template, rollback_file_dir)
-        header = "# Rollback information saved in " + rollback_info_file if rollback_info_file else ""
+        rollback_filename_template = \
+            common.generate_rollback_filename_template(
+                args.rollback_info_file_suffix
+            )
+        rollback_info_file = persist_rollback_info(
+            enabled_deployments,
+            rollback_filename_template,
+            rollback_file_dir
+        )
+        header = "# Rollback information saved in " \
+            + rollback_info_file if rollback_info_file else ""
 
-        undeploy_script = cli_output.generate_undeploy_script(enabled_deployments)
+        undeploy_script = cli_output.generate_undeploy_script(
+            enabled_deployments
+        )
 
         if controller.domain:
-             map_deployments_server_groups(archives, mapping_file)
+            map_deployments_server_groups(archives, mapping_file)
     else:
         undeploy_script = generate_undeploy_script(args, archives)
 
@@ -68,24 +89,36 @@ def generate_deploy_script(args, rollback_file_dir=None):
 
     return "{0}\n{1}\n{2}".format(header, undeploy_script, deploy_script)
 
-def persist_rollback_info(deployments, rollback_filename_template="rollback-info_", rollback_file_dir=None):
-    """Write name, runtime_name and server group of all enabled deployments to be replaced to a file named rollback-info_<timestamp>."""
+
+def persist_rollback_info(deployments,
+                          rollback_filename_template="rollback-info_",
+                          rollback_file_dir=None):
+    """Write name, runtime_name and server group of all enabled
+    deployments to be replaced to a file named rollback-info_<timestamp>.
+    """
     if not deployments:
         return
 
-    directory = rollback_file_dir if rollback_file_dir else os.path.dirname(os.path.abspath(__file__))
+    directory = rollback_file_dir if rollback_file_dir else \
+        os.path.dirname(os.path.abspath(__file__))
 
-    rollback_info_file = directory + os.path.sep + rollback_filename_template + str(int(round(time.time() * 1000)))
+    rollback_info_file = directory + os.path.sep + \
+        rollback_filename_template + str(int(round(time.time() * 1000)))
     deployment_line_template = "{0} {1} {2}\n"
     rollback_info = ""
 
     for deployment in deployments:
-        line = deployment_line_template.format(deployment.name, deployment.runtime_name, deployment.server_group)
+        line = deployment_line_template.format(
+            deployment.name,
+            deployment.runtime_name,
+            deployment.server_group
+        )
         rollback_info += line
 
     common.write_to_file(rollback_info_file, rollback_info)
 
     return rollback_info_file
+
 
 def generate_undeploy_script(args, archives=[]):
     if args.skip_undeploy:
@@ -94,6 +127,7 @@ def generate_undeploy_script(args, archives=[]):
         return cli_output.generate_undeploy_pattern(args.undeploy_pattern)
     else:
         return cli_output.generate_undeploy_script(archives, args.undeploy_tag)
+
 
 def map_deployments_server_groups(archives, mapping_file):
     mapping = read_server_group_mapping(mapping_file)
--- deploy/deploy_tests.py
+++ deploy/deploy_tests.py
@@ -11,90 +11,100 @@ from mock import ANY
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 class TestDeploy(unittest.TestCase):
 
-  def test_generate_deploy_script_emptyParams_shouldReturnEmptyString(self):
-      expected_script = ""
-      args = MagicMock()
+    def test_generate_deploy_script_emptyParams_shouldReturnEmptyString(self):
+        expected_script = ""
+        args = MagicMock()
 
-      script = deploy.generate_undeploy_script(args)
+        script = deploy.generate_undeploy_script(args)
 
-      self.assertEqual(script, expected_script)
+        self.assertEqual(script, expected_script)
 
-  def test_generate_deploy_script_skipUndeployTrue_shouldReturnEmptyString(self):
-      expected_script = ""
-      args = MagicMock()
-      args.skip_undeploy = True
+    def test_generate_deploy_script_skipUndeployTrue_ReturnEmptyString(self):
+        expected_script = ""
+        args = MagicMock()
+        args.skip_undeploy = True
 
-      script = deploy.generate_undeploy_script(args)
+        script = deploy.generate_undeploy_script(args)
 
-      self.assertEqual(script, expected_script)
+        self.assertEqual(script, expected_script)
 
-  def test_generate_deploy_script_undeployPattern_shouldReturnUndeployString(self):
-      expected_script = "undeploy --name=system-* --keep-content"
-      args = MagicMock()
-      args.undeploy_pattern = "system-*"
-      args.skip_undeploy = False
+    def test_generate_deploy_script_undeployPattern_ReturnUndeployString(self):
+        expected_script = "undeploy --name=system-* --keep-content"
+        args = MagicMock()
+        args.undeploy_pattern = "system-*"
+        args.skip_undeploy = False
 
-      script = deploy.generate_undeploy_script(args)
+        script = deploy.generate_undeploy_script(args)
 
-      self.assertEqual(script, expected_script)
+        self.assertEqual(script, expected_script)
 
-  def test_generate_deploy_script_withArchives_shouldReturnUndeployString(self):
-      expected_script = "\nundeploy system-v1.2.3 --keep-content"
-      args = MagicMock()
-      args.skip_undeploy = False
-      args.undeploy_pattern = None
-      args.undeploy_tag = None
+    def test_generate_deploy_script_withArchives_ReturnUndeployString(self):
+        expected_script = "\nundeploy system-v1.2.3 --keep-content"
+        args = MagicMock()
+        args.skip_undeploy = False
+        args.undeploy_pattern = None
+        args.undeploy_tag = None
 
-      archives = [Deployment("system-v1.2.3", "system.war")]
+        archives = [Deployment("system-v1.2.3", "system.war")]
 
-      script = deploy.generate_undeploy_script(args, archives)
+        script = deploy.generate_undeploy_script(args, archives)
 
-      self.assertEqual(script, expected_script)
+        self.assertEqual(script, expected_script)
 
-  @patch("deploy.common.write_to_file", MagicMock())
-  def test_persist_rollback_info_emptyDeploymentList_shouldNotWriteFile(self):
-      deployments = []
+    @patch("deploy.common.write_to_file", MagicMock())
+    def test_persist_rollback_info_emptyDeploymentList_DoNotWriteFile(self):
+        deployments = []
 
-      deploy.persist_rollback_info(deployments)
+        deploy.persist_rollback_info(deployments)
 
-      deploy.common.write_to_file.assert_not_called()
+        deploy.common.write_to_file.assert_not_called()
 
-  @patch("deploy.common.write_to_file", MagicMock())
-  def test_persist_rollback_info_NullDeploymentList_shouldNotWriteFile(self):
-      deployments = None
+    @patch("deploy.common.write_to_file", MagicMock())
+    def test_persist_rollback_info_NullDeploymentList_shouldNotWriteFile(self):
+        deployments = None
 
-      deploy.persist_rollback_info(deployments)
+        deploy.persist_rollback_info(deployments)
 
-      deploy.common.write_to_file.assert_not_called()
+        deploy.common.write_to_file.assert_not_called()
 
-  @patch("deploy.common.write_to_file", MagicMock())
-  def test_persist_rollback_info_oneDeployment_shouldWriteFile(self):
-      deployments = [Deployment("abc", "abc.war", server_group="group")]
+    @patch("deploy.common.write_to_file", MagicMock())
+    def test_persist_rollback_info_oneDeployment_shouldWriteFile(self):
+        deployments = [Deployment("abc", "abc.war", server_group="group")]
 
-      deploy.persist_rollback_info(deployments)
+        deploy.persist_rollback_info(deployments)
 
-      deploy.common.write_to_file.assert_called_with(ANY, "abc abc.war group\n")
+        deploy.common.write_to_file.assert_called_with(
+            ANY,
+            "abc abc.war group\n"
+        )
 
-  @patch("deploy.common.write_to_file", MagicMock())
-  def test_persist_rollback_info_twoDeployments_shouldWriteFile(self):
-      deployments = [Deployment("abc", "abc.war", server_group="group"), Deployment("cba-v5.2.0", "cba.war", server_group="pourg")]
+    @patch("deploy.common.write_to_file", MagicMock())
+    def test_persist_rollback_info_twoDeployments_shouldWriteFile(self):
+        deployments = [
+            Deployment("abc", "abc.war", server_group="group"),
+            Deployment("cba-v5.2.0", "cba.war", server_group="pourg")
+        ]
 
-      deploy.persist_rollback_info(deployments)
+        deploy.persist_rollback_info(deployments)
 
-      deploy.common.write_to_file.assert_called_with(ANY, "abc abc.war group\ncba-v5.2.0 cba.war pourg\n")
+        deploy.common.write_to_file.assert_called_with(
+            ANY,
+            "abc abc.war group\ncba-v5.2.0 cba.war pourg\n"
+        )
 
 
 # -- "Acceptance" Tests:
 
-  @patch("deploy.common.fetch_enabled_deployments", MagicMock(return_value=[Deployment("abc-v1.0.0", "abc.war", server_group="group")]))
-  @patch("deploy.common.read_from_file", MagicMock(return_value=["abc.war=group"]))
-  @patch("os.path.isfile", MagicMock(return_value=True))
-  @patch("__main__.deploy.persist_rollback_info", MagicMock(return_value=current_dir + os.sep + "rollback-info_test"))
-  @patch("deploy.common.read_archive_files", MagicMock(return_value=[Deployment("abc-v1.2.3", "abc.war", path=current_dir + os.sep + "v1.2.3" + os.sep + "abc.war")]))
-  def test_generate_deploy_script_receiving_data_from_controller(self):
-      expected_script="""\
+    @patch("deploy.common.fetch_enabled_deployments", MagicMock(return_value=[Deployment("abc-v1.0.0", "abc.war", server_group="group")]))
+    @patch("deploy.common.read_from_file", MagicMock(return_value=["abc.war=group"]))
+    @patch("os.path.isfile", MagicMock(return_value=True))
+    @patch("__main__.deploy.persist_rollback_info", MagicMock(return_value=current_dir + os.sep + "rollback-info_test"))
+    @patch("deploy.common.read_archive_files", MagicMock(return_value=[Deployment("abc-v1.2.3", "abc.war", path=current_dir + os.sep + "v1.2.3" + os.sep + "abc.war")]))
+    def test_generate_deploy_script_receiving_data_from_controller(self):
+        expected_script = """\
 # Rollback information saved in {0}rollback-info_test
 
 undeploy abc-v1.0.0 --keep-content --all-relevant-server-groups
@@ -102,64 +112,82 @@ undeploy abc-v1.0.0 --keep-content --all-relevant-server-groups
 deploy {1} --runtime-name=abc.war --name=abc-v1.2.3 --server-groups=group\
 """.format(current_dir + os.sep,
            current_dir + os.sep + "v1.2.3" + os.sep + "abc.war")
-      args = MagicMock()
-      args.path = current_dir + os.sep + "v1.2.3"
-      args.undeploy_pattern = None
-      args.undeploy_tag = None
 
-      mock_controller = MagicMock()
-      mock_controller.domain = True
-      deploy.common.initialize_controller = MagicMock(return_value=mock_controller)
+        args = MagicMock()
+        args.path = current_dir + os.sep + "v1.2.3"
+        args.undeploy_pattern = None
+        args.undeploy_tag = None
 
-      script = deploy.generate_deploy_script(args)
+        mock_controller = MagicMock()
+        mock_controller.domain = True
+        deploy.common.initialize_controller = MagicMock(
+            return_value=mock_controller
+        )
 
-      self.assertEqual(script, expected_script)
+        script = deploy.generate_deploy_script(args)
 
-  @patch("deploy.common.fetch_enabled_deployments", MagicMock(return_value=[]))
-  @patch("deploy.common.read_from_file", MagicMock(return_value=["abc.war=group"]))
-  @patch("os.path.isfile", MagicMock(return_value=True))
-  @patch("deploy.common.read_archive_files", MagicMock(return_value=[Deployment("abc-v1.2.3", "abc.war", path=current_dir + os.sep + "v1.2.3" + os.sep + "abc.war")]))
-  def test_generate_deploy_script_noEnabledDeployments_shouldNotPrintRollbackHeader(self):
-      expected_script="""
+        self.assertEqual(script, expected_script)
+
+    @patch("deploy.common.fetch_enabled_deployments",
+           MagicMock(return_value=[]))
+    @patch("deploy.common.read_from_file",
+           MagicMock(return_value=["abc.war=group"]))
+    @patch("os.path.isfile", MagicMock(return_value=True))
+    @patch("deploy.common.read_archive_files",
+           MagicMock(return_value=[
+            Deployment("abc-v1.2.3",
+                       "abc.war",
+                       path=current_dir + os.sep +
+                       "v1.2.3" + os.sep + "abc.war")
+                       ]))
+    def test_generate_deploy_script_noEnabledDeployments_DoNotPrintRollbackHeader(self):
+        expected_script = """
 
 
 deploy {1} --runtime-name=abc.war --name=abc-v1.2.3 --server-groups=group\
 """.format(current_dir + os.sep,
            current_dir + os.sep + "v1.2.3" + os.sep + "abc.war")
-      args = MagicMock()
-      args.path = current_dir + os.sep + "v1.2.3"
-      args.undeploy_pattern = None
-      args.undeploy_tag = None
+        args = MagicMock()
+        args.path = current_dir + os.sep + "v1.2.3"
+        args.undeploy_pattern = None
+        args.undeploy_tag = None
 
-      mock_controller = MagicMock()
-      mock_controller.domain = True
-      deploy.common.initialize_controller = MagicMock(return_value=mock_controller)
+        mock_controller = MagicMock()
+        mock_controller.domain = True
+        deploy.common.initialize_controller = MagicMock(
+            return_value=mock_controller
+        )
 
-      script = deploy.generate_deploy_script(args)
+        script = deploy.generate_deploy_script(args)
 
-      self.assertEqual(script, expected_script)
+        self.assertEqual(script, expected_script)
 
-
-  @patch("deploy.common.read_archive_files", MagicMock(return_value=[Deployment("system-v1.2.3", "system.war", path=current_dir + os.sep + "v1.2.3" + os.sep + "system.war")]))
-  def test_generate_deploy_script_skip_local_undeploy(self):
-      expected_script = """
+    @patch("deploy.common.read_archive_files",
+           MagicMock(return_value=[
+            Deployment("system-v1.2.3",
+                       "system.war",
+                       path=current_dir + os.sep + "v1.2.3" + os.sep +
+                       "system.war")
+                    ]))
+    def test_generate_deploy_script_skip_local_undeploy(self):
+        expected_script = """
 
 undeploy system-sometag --keep-content
 
 deploy {0}system.war --runtime-name=system.war --name=system-v1.2.3\
 """.format(current_dir + os.sep + "v1.2.3" + os.sep)
 
-      args = MagicMock()
-      args.skip_undeploy = False
-      args.path = current_dir + os.sep + "v1.2.3"
-      args.undeploy_pattern = None
-      args.undeploy_tag = "sometag"
+        args = MagicMock()
+        args.skip_undeploy = False
+        args.path = current_dir + os.sep + "v1.2.3"
+        args.undeploy_pattern = None
+        args.undeploy_tag = "sometag"
 
-      deploy.common.initialize_controller = MagicMock(return_value=None)
+        deploy.common.initialize_controller = MagicMock(return_value=None)
 
-      script = deploy.generate_deploy_script(args)
+        script = deploy.generate_deploy_script(args)
 
-      self.assertEqual(script, expected_script)
+        self.assertEqual(script, expected_script)
 
 if __name__ == '__main__':
     unittest.main()
